72e774db835d95157d0535d8f83637090b26607b
Igor Anczykowski <i.anczykowski@stud.elka.pw.edu.pl>
2021-05-18 13:41:28 +0200

    Added api_key auth

--- app/evox_messages/views.py
+++ app/evox_messages/views.py
@@ -1,17 +1,30 @@
+from django.forms import model_to_dict
 from django.http import HttpResponse
+from django.http.response import JsonResponse
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework_api_key.permissions import HasAPIKey
+from evox_messages.models import Message
 
 
-def messages(request):
-    return HttpResponse('Return all messages')
+@api_view(['GET'])
+def message_show(request, id: int):
+    message = model_to_dict(Message.objects.get(pk=id))
+    return JsonResponse(message)
 
 
+@api_view(['GET'])
+@permission_classes([HasAPIKey])
 def message_edit(request, id: int):
     return HttpResponse(f'Edit message with id={id}')
 
 
+@api_view(['GET'])
+@permission_classes([HasAPIKey])
 def message_delete(request, id: int):
     return HttpResponse(f'Edit message with id={id}')
 
 
+@api_view(['GET'])
+@permission_classes([HasAPIKey])
 def message_new(request):
     return HttpResponse('Create new message')
