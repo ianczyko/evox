4d2d51c8dd7afa6c78f643df99f7396b3880e138
Igor Anczykowski <i.anczykowski@stud.elka.pw.edu.pl>
2021-05-18 19:52:34 +0200

    Added unit tests for Message model

--- app/evox_messages/tests.py
+++ app/evox_messages/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
+from evox_messages.models import Message
 
-# Create your tests here.
+
+class MessageModelTestCase(TestCase):
+    def setUp(self):
+        Message.objects.create(content='msg1')
+        Message.objects.create(content='msg2')
+
+    def test_id_incremented_create(self):
+        """Message model creation"""
+        msg1 = Message.objects.get(pk=1)
+        msg2 = Message.objects.get(pk=2)
+        self.assertEqual(msg1.content, 'msg1')
+        self.assertEqual(msg1.view_count, 0)
+        self.assertEqual(msg2.content, 'msg2')
+        self.assertEqual(msg2.view_count, 0)
